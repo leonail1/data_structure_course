87b5de8eb8240cfa3584d5134be1c7c410dbcab5
Leo <3190916418@qq.com>
2023-09-12 18:14:34 +0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "线性表_数组描述.h"
+using namespace std;
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
---
+++ 线性表_数组描述.h
@@ -0,0 +1,10 @@
+//
+// Created by 木林 on 2023/9/12.
+//
+
+#ifndef UNTITLED_线性表_数组描述_H
+#define UNTITLED_线性表_数组描述_H
+
+
+
+#endif //UNTITLED_线性表_数组描述_H
